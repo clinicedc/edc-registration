9c5c73f4209f565e580c510c25f849922f805a84
erikvw <ew2789@gmail.com>
2018-07-04 14:49:09 +0200

    flake8

--- edc_registration/model_mixins/__init__.py
+++ edc_registration/model_mixins/__init__.py
@@ -1,2 +1,3 @@
 from .registered_subject_model_mixin import RegisteredSubjectModelMixin
-from .updates_or_creates_registered_subject_model_mixin import UpdatesOrCreatesRegistrationModelMixin
+from .updates_or_creates_registered_subject_model_mixin import \
+    UpdatesOrCreatesRegistrationModelMixin
