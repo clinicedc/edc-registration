a69001f110164b961cfd36250c82b9a0d9c96b08
erikvw <ew2789@gmail.com>
2019-03-04 11:21:30 -0600

    setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
     VERSION = f.read()
 
-tests_require = []
+tests_require = ['edc-device']
 with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
     for line in f:
         tests_require.append(line.strip())
