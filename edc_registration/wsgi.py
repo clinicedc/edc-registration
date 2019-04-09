c3f3ed63ec9ba5cf583abc0061fcb9de424f1a59
erikvw <ew2789@gmail.com>
2019-04-09 09:02:13 -0500

    remove admin templates, tox

--- edc_registration/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edc_registration.settings")
-
-application = get_wsgi_application()
