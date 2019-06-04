71cfe74cdfc992cd5b435d7639a2f05629b1ddfe
erikvw <ew2789@gmail.com>
2019-06-04 14:19:07 -0500

    add default ordering

--- edc_registration/utils.py
+++ edc_registration/utils.py
@@ -9,14 +9,14 @@ def get_registered_subject_model():
 def get_registered_subject(subject_identifier):
     registered_subject = None
     try:
-        model_cls = django_apps.get_model(
-            "edc_registration.registeredsubject")
+        model_cls = django_apps.get_model("edc_registration.registeredsubject")
     except LookupError:
         pass
     else:
         try:
             registered_subject = model_cls.objects.get(
-                subject_identifier=subject_identifier)
+                subject_identifier=subject_identifier
+            )
         except ObjectDoesNotExist:
             pass
     return registered_subject
