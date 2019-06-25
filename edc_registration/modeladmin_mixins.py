496569f336049a52f25aa7b087f4469d600fff94
erikvw <ew2789@gmail.com>
2019-06-25 16:37:03 -0500

    hide PII if no perms on changelist and form

--- edc_registration/modeladmin_mixins.py
+++ edc_registration/modeladmin_mixins.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from edc_model_admin import audit_fields
 from edc_model_admin.dashboard import ModelAdminSubjectDashboardMixin
+from django.core.exceptions import ObjectDoesNotExist
+from edc_permissions.constants.group_names import PII_VIEW, PII
 
 
 class RegisteredSubjectModelAdminMixin(
@@ -12,6 +14,24 @@ class RegisteredSubjectModelAdminMixin(
     date_hierarchy = "registration_datetime"
 
     instructions = []
+
+    def show_pii(self, request):
+        try:
+            show_pii = request.user.groups.get(name__in=[PII, PII_VIEW])
+        except ObjectDoesNotExist:
+            show_pii = None
+        return show_pii
+
+    def get_fieldsets(self, request, obj=None):
+        """
+        Hook for specifying fieldsets.
+        """
+        if self.fieldsets:
+            if self.show_pii(request):
+                return self.fieldsets
+            else:
+                return self.fieldsets_no_pii
+        return [(None, {'fields': self.get_fields(request, obj)})]
 
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = super().get_readonly_fields(request, obj=obj)
@@ -38,39 +58,75 @@ class RegisteredSubjectModelAdminMixin(
         )
 
     def get_list_display(self, request):
-        return [
-            "subject_identifier",
-            "dashboard",
-            "first_name",
-            "initials",
-            "gender",
-            "subject_type",
-            "sid",
-            "registration_status",
-            "site",
-            "user_created",
-            "created",
-        ] + list(super().get_list_display(request))
+
+        if self.show_pii(request):
+            list_display = [
+                "subject_identifier",
+                "dashboard",
+                "first_name",
+                "initials",
+                "gender",
+                "subject_type",
+                "sid",
+                "registration_status",
+                "site",
+                "user_created",
+                "created",
+            ]
+        else:
+            list_display = [
+                "subject_identifier",
+                "dashboard",
+                "gender",
+                "subject_type",
+                "sid",
+                "registration_status",
+                "site",
+                "user_created",
+                "created",
+            ]
+        return list_display + list(super().get_list_display(request))
 
     def get_list_filter(self, request):
-        return [
-            "subject_type",
-            "registration_status",
-            "screening_datetime",
-            "registration_datetime",
-            "gender",
-            "site",
-            "hostname_created",
-        ] + list(super().get_list_filter(request))
+        if self.show_pii(request):
+            list_filter = [
+                "subject_type",
+                "registration_status",
+                "screening_datetime",
+                "registration_datetime",
+                "gender",
+                "site",
+                "hostname_created",
+            ]
+        else:
+            list_filter = [
+                "subject_type",
+                "registration_status",
+                "screening_datetime",
+                "registration_datetime",
+                "site",
+                "hostname_created",
+            ]
+        return list_filter + list(super().get_list_filter(request))
 
     def get_search_fields(self, request):
-        return [
-            "subject_identifier",
-            "first_name",
-            "initials",
-            "sid",
-            "identity",
-            "id",
-            "screening_identifier",
-            "registration_identifier",
-        ] + list(super().get_search_fields(request))
+        if self.show_pii(request):
+            search_fields = [
+                "subject_identifier",
+                "first_name",
+                "initials",
+                "sid",
+                "identity",
+                "id",
+                "screening_identifier",
+                "registration_identifier",
+            ]
+        else:
+            search_fields = [
+                "subject_identifier",
+                "sid",
+                "id",
+                "screening_identifier",
+                "registration_identifier",
+            ]
+        return search_fields + list(super().get_search_fields(request))
