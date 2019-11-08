59e47ce4ee7081899cce2c1549049c93770c07a1
erikvw <ew2789@gmail.com>
2019-11-07 23:30:57 -0600

    use get model utils

--- edc_registration/utils.py
+++ edc_registration/utils.py
@@ -2,14 +2,18 @@ from django.apps import apps as django_apps
 from django.core.exceptions import ObjectDoesNotExist
 
 
+def get_registered_subject_model_name():
+    return "edc_registration.registeredsubject"
+
+
 def get_registered_subject_model():
-    return django_apps.get_model("edc_registration.registeredsubject")
+    return django_apps.get_model(get_registered_subject_model_name())
 
 
 def get_registered_subject(subject_identifier):
     registered_subject = None
     try:
-        model_cls = django_apps.get_model("edc_registration.registeredsubject")
+        model_cls = get_registered_subject_model()
     except LookupError:
         pass
     else:
