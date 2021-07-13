2c1f550ef8ed5e4421867e12599be5f1eb14dc9f
erikvw <ew2789@gmail.com>
2021-07-12 23:27:23 -0500

    use func to get model cls

--- edc_registration/__init__.py
+++ edc_registration/__init__.py
@@ -1,1 +1,1 @@
-from .utils import get_registered_subject, get_registered_subject_model
+from .utils import get_registered_subject, get_registered_subject_model_cls
--- edc_registration/admin.py
+++ edc_registration/admin.py
@@ -3,10 +3,10 @@ from edc_model_admin import SimpleHistoryAdmin, audit_fieldset_tuple
 
 from .admin_site import edc_registration_admin
 from .modeladmin_mixins import RegisteredSubjectModelAdminMixin
-from .models import RegisteredSubject
+from .utils import get_registered_subject_model_cls
 
 
-@admin.register(RegisteredSubject, site=edc_registration_admin)
+@admin.register(get_registered_subject_model_cls(), site=edc_registration_admin)
 class RegisteredSubjectAdmin(RegisteredSubjectModelAdminMixin, SimpleHistoryAdmin):
 
     ordering = ("subject_identifier",)
